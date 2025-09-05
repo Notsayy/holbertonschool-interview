1bbf3cdd0e5d82e2866d79aab95c9dae01ba0139
Notsayy <jtertre25@gmail.com>
2025-09-05 20:24:12 +0200

    Docstring

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Module 0-lockboxes
+This module provides a function that determines if all the boxes can be opened.
+"""
+
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
